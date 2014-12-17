50f73ea25e6b12d7c8d2806c14a7a53ebd757630
yama3 <yamaguchi@alphaversion.co.jp>
2014-12-17 09:28:52 +0900

    convert tab to space

--- LocalizedString.swift
+++ LocalizedString.swift
@@ -2,12 +2,12 @@
 //  LocalizedString.swift
 //
 //  Created by nya.
-//	Copyright 2014 Alpha Version Inc.
+//  Copyright 2014 Alpha Version Inc.
 //
 
 import Foundation
 
 
 func L(key: String, comment: String = "") -> String {
-	return NSLocalizedString(key, comment: comment)
+    return NSLocalizedString(key, comment: comment)
 }
